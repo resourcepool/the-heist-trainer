dfd4e22a9f653a063d9a74022055ec3fffc581ca
Antoine Lebel <antoleb@hotmail.fr>
2022-05-23 15:34:06 +0200

    deploy S3 website

--- website/src/environments/environment.prod.ts
+++ website/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.1.2:3000',
+  // apiUrl: 'http://192.168.1.2:3000',
+  apiUrl: 'https://api-secret.taki.events',
   adminPassword: 'messi',
 };
